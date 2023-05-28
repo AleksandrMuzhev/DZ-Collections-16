02348ed2606d2b61d2d7f4ab3965a3206b3ab73d
Aleksand Muzhev <muzhev1990@mail.ru>
2023-05-29 00:17:24 +0500

    Finish Second Task

--- src/main/java/ru/netology/Game.java
+++ src/main/java/ru/netology/Game.java
@@ -1,39 +1,42 @@
 package ru.netology;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Game {
-    List<Player> players = new ArrayList<>();
+    HashMap<String, Integer> players = new HashMap<>();
 
     public void register(Player player) {
-        players.add(player);
+        players.put(player.getName(), player.getStrength());
     }
 
     public int round(String playerName1, String playerName2) {
-        Player player1 = findByName(playerName1);
-        Player player2 = findByName(playerName2);
+        Integer player1 = players.get(playerName1);
+        Integer player2 = players.get(playerName2);
         if (player1 == null) {
             throw new NotRegisteredException("Element with name: " + player1 + " not registered");
         }
         if (player2 == null) {
             throw new NotRegisteredException("Element with name: " + player2 + " not registered");
         }
-        if (player1.getStrength() > player2.getStrength()) {
+        if (player1 > player2) {
             return 1;
         }
-        if (player1.getStrength() < player2.getStrength()) {
+        if (player1 < player2) {
             return 2;
         }
         return 0;
     }
 
-    public Player findByName(String name) {
-        for (Player player : players) {
-            if (player.getName() == name) {
-                return player;
-            }
-        }
-        return null;
-    }
 }
+//
+//    public Player findByName(String name) {
+//        for (String player : players.keySet()) {
+//            if (player. () == name){
+//                return player;
+//            }
+//        }
+//        return null;
+//    }
+//}
